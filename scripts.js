73a2614a5e09cdaccc6805fedf6328b41cdeead3
Nelvinom <omondinelvine884@gmail.com>
2020-02-17 10:19:06 +0300

    commit

--- scripts.js
+++ scripts.js
@@ -5,8 +5,10 @@ function output() {
     var mm = parseInt(month);
     var year = document.getElementById("year").value;
     var yy = parseInt(year);
+    var century = document.getElementById("century").value;
+    var cc = parseInt(century)
     var cc = (yy - 1) / 100 + 1;
-    var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+    var result = parseInt(cc + "-" + yy + "-" + mm + "-" + dd);
     var boy = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var girl = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     if (document.getElementById("gender").checked){
